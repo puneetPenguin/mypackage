0b6c349119677d7bc0bc3a85f7821de8d3843a52
Puneet Bisht <90336304+puneetPenguin@users.noreply.github.com>
2025-02-20 10:45:38 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mypackage",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],
+    description="A simple custom Python package",
+    author="Your Name",
+    author_email="your.email@example.com",
+    url="https://github.com/yourusername/mypackage",
+)
